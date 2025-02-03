f0ca651ec1638d94898a08fb2d67a1bceec35b7f
anna-leite <anna.leite@laplateforme.io>
2025-02-03 13:40:12 +0100

    job 3

--- jour01.py
+++ jour01.py
@@ -2,12 +2,19 @@ class Operation():
     def __init__(self, nombre1, nombre2):
         self.nombre1 = nombre1
         self.nombre2 = nombre2
+    
+    def addition(self):
+        return self.nombre1 + self.nombre2
+    
+    def __str__(self):
+        return f"{self.nombre1}, {self.nombre2}"
 
 
-#instancier un objet
+#instancie un objet
 objet = Operation(12, 3)
 
-# impression d'un attribut de l'objet
-print(f"Le nombre1 est {objet.nombre1}")
-print(f"Le nombre1 est {objet.nombre2}")
+# appelle une méthode sur un objet instancié
+resultat = objet.addition()
+
+print(resultat)
         
